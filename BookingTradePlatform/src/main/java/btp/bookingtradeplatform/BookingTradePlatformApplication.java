4daeeb7dceb0e508665c4c55697ae3a1f58f2088
Ho4FireDragon <anhoa1794@gmail.com>
2025-05-30 14:36:40 +0700

    Start project

---
+++ BookingTradePlatform/src/main/java/btp/bookingtradeplatform/BookingTradePlatformApplication.java
@@ -0,0 +1,13 @@
+package btp.bookingtradeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingTradePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingTradePlatformApplication.class, args);
+    }
+
+}
---
+++ BookingTradePlatform/src/test/java/btp/bookingtradeplatform/BookingTradePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package btp.bookingtradeplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTradePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
